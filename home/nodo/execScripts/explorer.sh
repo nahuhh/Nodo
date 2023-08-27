ab7b6e5a23c52639ff7e8e5691cd5d82ac6e7542
interloper <15019654+intr-cx@users.noreply.github.com>
2023-08-27 11:42:50 +0200

    fix block explorer scripts (RPC login, etc)

--- home/nodo/execScripts/explorer.sh
+++ home/nodo/execScripts/explorer.sh
@@ -4,7 +4,13 @@
 . /home/nodo/common.sh
 
 exploc=/home/nodo/onion-monero-blockchain-explorer/build/
-DATA_DIR=$(getvar "data_dir")
-MONEROD="127.0.0.1:"$(getvar "monero_public_port")
+DATA_DIR=$(getvar "data_dir")/lmdb
+MONERO_RPC_PORT=$(getvar "monero_rpc_port")
+MONEROD="127.0.0.1:$MONERO_RPC_PORT}"
+RPC_ENABLED=$(getvar "rpc_enabled")
+if [ "$RPC_ENABLED" == "TRUE" ]; then
+	RPCp=$(getvar "rpcp")
+	RPCu=$(getvar "rpcu")
+fi
 
-eval "$exploc/xmrblocks -d $MONEROD --enable-json-api=1 -b $DATA_DIR" >/dev/null
+eval "$exploc/xmrblocks -d $MONEROD ${RPCp:+--daemon-login $RPCp:$RPCu} --enable-json-api=1 -b $DATA_DIR"
