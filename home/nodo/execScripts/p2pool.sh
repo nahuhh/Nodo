839f74483b27eba52abdc07fbb93bbcb83b4f308
interloper <15019654+intr-cx@users.noreply.github.com>
2023-11-01 15:39:54 +0100

    Fix p2pool script, use rpc credentials

--- home/nodo/execScripts/p2pool.sh
+++ home/nodo/execScripts/p2pool.sh
@@ -4,5 +4,18 @@
 . /home/nodo/common.sh
 
 WALLET_ADDRESS=$(getvar "mining.address")
+{
+	read -r WALLET_ADDRESS
+	read -r RPC_ENABLED
+	read -r RPCP
+	read -r RPCU
+	read -r RPC_PORT
+} < <(
+jq -r '.config | .mining.address, .rpc_enabled, .rpcp, .rpcu, .monero_rpc_port' $CONFIG_FILE
+)
 
-eval /home/nodo/p2pool/build/p2pool --mini --host 127.0.0.1 --wallet "$WALLET_ADDRESS"
+if [ -n "$RPC_ENABLED" ]; then
+	eval /home/nodo/p2pool/build/p2pool --mini --host 127.0.0.1 --rpc-port "$RPC_PORT" --wallet "$WALLET_ADDRESS" --rpc-login "$RPCU:$RPCP"
+else
+	eval /home/nodo/p2pool/build/p2pool --mini --host 127.0.0.1 --rpc-port "$RPC_PORT" --wallet "$WALLET_ADDRESS"
+fi
