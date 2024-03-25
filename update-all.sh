3c529a12274c19c2cf3848a54fb249528cc40b5b
Pass Automated Testing Suite <15019654+intr-cx@users.noreply.github.com>
2024-03-25 09:54:53 +0100

    Reset ownership after build

--- update-all.sh
+++ update-all.sh
@@ -5,6 +5,7 @@
 . /home/nodo/common.sh
 
 cd /home/nodo && /home/nodo/update-nodo.sh
+chown nodo:nodo -R EmbeddedUI monero monero-lws p2pool xmrig onion-monero-block-explorer
 sudo -u nodo bash /home/nodo/update-monero.sh
 sudo -u nodo bash /home/nodo/update-monero-lws.sh
 sudo -u nodo bash /home/nodo/update-p2pool.sh
