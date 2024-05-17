04947aed71a6e42f8c6ed2400016023f52f72245
Your Name <15019654+intr-cx@users.noreply.github.com>
2024-05-17 10:27:07 +0200

    Fix monero-lws-daemon bin location

--- home/nodo/execScripts/monero-lws.sh
+++ home/nodo/execScripts/monero-lws.sh
@@ -3,9 +3,7 @@
 #shellcheck source=home/nodo/common.sh
 . /home/nodo/common.sh
 
-lwsloc=/home/nodo/monero-lws/build/src/
-
 DATA_DIR=$(getvar "data_dir")/light_wallet_server
 PORT=8135
 
-eval "${lwsloc}monero-lws-daemon --daemon tcp://127.0.0.1:18082 --db-path $DATA_DIR --admin-rest-server http://127.0.0.1:${PORT}/admin --rest-server http://127.0.0.1:${PORT}/basic"
+eval "/home/nodo/bin/monero-lws-daemon --daemon tcp://127.0.0.1:18082 --db-path $DATA_DIR --admin-rest-server http://127.0.0.1:${PORT}/admin --rest-server http://127.0.0.1:${PORT}/basic"
