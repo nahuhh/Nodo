f7e0881395df7cac83cdd506e3d468e6abbae769
Interloper <15019654+intr-cx@users.noreply.github.com>
2024-09-16 12:25:32 +0200

    Fix typo

--- home/nodo/execScripts/explorer.sh
+++ home/nodo/execScripts/explorer.sh
@@ -12,9 +12,9 @@ if [ "$RPC_ENABLED" == "TRUE" ]; then
 	read -r RPCp
 fi
 } < <(
-jq -r '.config | .data_dir, .monero_rpc_port, .rpc_enabled, .rpcu, .rpcp' < $CONFIG_FILE
+jq -r '.config | .data_dir, .monero_rpc_port, .rpc_enabled, .rpcu, .rpcp' < "$CONFIG_FILE"
 )
 
-MONEROD="127.0.0.1:$MONERO_RPC_PORT}"
+MONEROD="127.0.0.1:$MONERO_RPC_PORT"
 
 eval "/home/nodo/bin/xmrblocks --daemon-url=$MONEROD ${RPCp:+--daemon-login $RPCu:$RPCp} --enable-json-api=1 -b $DATA_DIR/lmdb"
