6bc64846af3f7d88521baa5d833eb79ac38f86c3
Interloper <15019654+intr-cx@users.noreply.github.com>
2024-09-28 11:30:29 +0200

    Run Xmrig as root + 1gb pages

--- home/nodo/execScripts/xmrig.sh
+++ home/nodo/execScripts/xmrig.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-eval /home/nodo/bin/xmrig -c /home/nodo/xmrig.json
+#shellcheck source=home/nodo/common.sh
+. /home/nodo/common.sh
+
+{
+read -r MONERO_PUBLIC_PORT
+read -r ADDRESS
+} < <(
+        jq '.config | .monero_public_port, .mining.address' $CONFIG_FILE
+)
+eval /home/nodo/bin/xmrig -o 127.0.0.1:"$MONERO_PUBLIC_PORT" --randomx-1gb-pages -a rx/0 -u "$ADDRESS" --daemon
