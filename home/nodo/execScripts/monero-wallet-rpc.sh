e73f512263ff4571dda292a00124ad9a1c0650d6
interloper <15019654+intr-cx@users.noreply.github.com>
2024-10-16 17:53:49 +0200

    Use the right port

--- home/nodo/execScripts/monero-wallet-rpc.sh
+++ home/nodo/execScripts/monero-wallet-rpc.sh
@@ -4,17 +4,17 @@
 . /home/nodo/common.sh
 
 {
-read -r MONERO_RPC_PORT
+read -r MONERO_PUBLIC_PORT
 read -r RPC_ENABLED
 read -r RPCu
 read -r RPCp
 } < <(
-jq -r '.config | .monero_rpc_port, .rpc_enabled, .rpcu, .rpcp' < $CONFIG_FILE
+jq -r '.config | .monero_public_port, .rpc_enabled, .rpcu, .rpcp' < $CONFIG_FILE
 )
 
 if [ "$RPC_ENABLED" == "TRUE" ]; then
 	rpc_args="--daemon-login=\"$RPCu:$RPCp\""
 fi
 
-eval /home/nodo/bin/monero-wallet-rpc --trusted-daemon --daemon-address 127.0.0.1:"$MONERO_RPC_PORT" \
+eval /home/nodo/bin/monero-wallet-rpc --trusted-daemon --daemon-address 127.0.0.1:"$MONERO_PUBLIC_PORT" \
 	"$rpc_args" --daemon-ssl disabled --disable-rpc-login --non-interactive --rpc-bind-port=34512 --wallet-dir /home/nodo
