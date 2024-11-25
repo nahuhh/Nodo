7944aecaae766a7d69d87687b8fd0c5156cb7e9a
interloper <15019654+intr-cx@users.noreply.github.com>
2024-11-25 19:31:09 +0100

    Enable 0-conf for Moneropay

--- home/nodo/execScripts/moneropay.sh
+++ home/nodo/execScripts/moneropay.sh
@@ -3,4 +3,5 @@
 #shellcheck source=home/nodo/common.sh
 . /home/nodo/common.sh
 
-/home/nodo/bin/moneropay -rpc-address="http://127.0.0.1:34512/json_rpc" -sqlite="file:///home/nodo/moneropay.sqlite" -bind="127.0.0.1:5000"
+
+/home/nodo/bin/moneropay -zero-conf=true -rpc-address="http://127.0.0.1:34512/json_rpc" -sqlite="file:///home/nodo/moneropay.sqlite" -bind="127.0.0.1:5000"
