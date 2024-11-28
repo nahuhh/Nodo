401271c54340125eaec5ad663cd7b633c8874847
interloper <15019654+intr-cx@users.noreply.github.com>
2024-11-28 19:25:03 +0100

    Properly set Nodo hostname to reflect the N6

---
+++ etc/init.d/friendlyelec_firstuse.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+### BEGIN INIT INFO
+# Provides:          friendlyelec
+# Required-Start:
+# Required-Stop:
+# Default-Start:
+# Default-Stop:
+# Short-Description:
+# Description:       Init board
+### END INIT INFO
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+BOARD=Nano-N6
+
+remove_unnecessary_network_settings() {
+    for f in $(ls /etc/network/interfaces.d); do
+        found=0
+        for ((i=0;i<${1};i++)); do
+            if [ "$f" = "eth${i}" ]; then
+                found=1
+            fi
+        done
+        if [ $found -eq 0 ]; then
+            rm -f /etc/network/interfaces.d/$f
+        fi
+    done
+}
+
+if [ ! -f /etc/firstuse ]; then
+    /usr/local/bin/gen-friendlyelec-release
+    . /etc/friendlyelec-release
+    /bin/echo ${BOARD} > /etc/hostname
+    /bin/sed -i "s/\(127.0.1.1\s*\).*/\1${BOARD}/g" /etc/hosts
+    /bin/hostname ${BOARD}
+
+    case ${BOARD} in
+    NanoPi-R5S*|NanoPi-R6S)
+        remove_unnecessary_network_settings 3
+        ;;
+    SOM-4418|NanoPi-R1|NanoPi-R1S-H3|NanoPi-R1S-H5|NanoPi-R4S|NanoPi-R4SE|NanoPi-R2S*|NanoPi-R2C|NanoPi-R2C-Plus|Core3328|NanoPi-R5C|NanoPC-T6*|NanoPi-R6C)
+        remove_unnecessary_network_settings 2
+        ;;
+    NanoPi-NEO-Air)
+        rm -f /etc/network/interfaces.d/eth*
+        ;;
+    *)
+        remove_unnecessary_network_settings 1
+        ;;
+    esac
+
+    ldconfig || true
+    /bin/echo "0" > /etc/firstuse
+fi
+
--- setup-nodo.sh
+++ setup-nodo.sh
@@ -185,6 +185,8 @@ chmod o+rx /home/nodo
 chmod o+rx /home/nodo/execScripts
 chmod 666 /home/nodo/variables/config.json
 
+hostname Nano-N6
+
 putvar 'i2p_port' '18081'
 sleep 5
 bash /home/nodo/setup-domains.sh
