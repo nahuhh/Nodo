8b0df6a941a3e5c283931f52e5d195d7ad5133c2
nahuhh <50635951+nahuhh@users.noreply.github.com>
2024-12-05 12:42:59 +0000

    monero: adjust flags

--- home/nodo/execScripts/monerod.sh
+++ home/nodo/execScripts/monerod.sh
@@ -24,31 +24,28 @@ read -r TOR_PORT
 read -r TOR_ADDRESS
 read -r DATA_DIR
 read -r SYNC_MODE
+read -r ZMQ_PUB
 } < <(
-	jq '.config | .monero_port, .monero_public_port, .rpc_enabled, .monero_rpc_port, .rpcu, .rpcp, .in_peers, .out_peers, .limit_rate_up, .limit_rate_down, .data_dir, .torproxy_enabled, .i2p_enabled, .i2p_port, .i2p_address, .tor_enabled, .tor_port, .tor_address, .data_dir, .sync_mode' $CONFIG_FILE
+	jq '.config | .monero_port, .monero_public_port, .rpc_enabled, .monero_rpc_port, .rpcu, .rpcp, .in_peers, .out_peers, .limit_rate_up, .limit_rate_down, .data_dir, .torproxy_enabled, .i2p_enabled, .i2p_port, .i2p_address, .tor_enabled, .tor_port, .tor_address, .data_dir, .sync_mode, .zmq_pub' $CONFIG_FILE
 )
 
 DEVICE_IP="0.0.0.0"
 
 #Start Monerod
 if [ "$TORPROXY_ENABLED" == "TRUE" ]; then
-	if [ "$I2P_ENABLED" == "TRUE" ]; then
-		cln_flags="--proxy=127.0.0.1:4447 "
-	else
-		cln_flags="--proxy=127.0.0.1:9050 "
-	fi
+	cln_flags="--proxy=127.0.0.1:9050 "
 fi
 
 if [ "$I2P_ENABLED" == "TRUE" ]; then
-	i2p_args="--tx-proxy=\"i2p,127.0.0.1:4447,64\" ${I2P_ADDRESS:+--anonymous-inbound=$I2P_ADDRESS,127.0.0.1:$I2P_PORT} "
+	i2p_args="--tx-proxy=i2p,127.0.0.1:4447,32,disable_noise ${I2P_ADDRESS:+--anonymous-inbound=$I2P_ADDRESS,127.0.0.1:$I2P_PORT} "
 fi
 
 if [ "$TOR_ENABLED" == "TRUE" ]; then
-	tor_args="--tx-proxy=\"tor,127.0.0.1:9050,64\" ${TOR_ADDRESS:+--anonymous-inbound=$TOR_ADDRESS:$TOR_PORT,127.0.0.1:$TOR_PORT} "
+	tor_args="--tx-proxy=tor,127.0.0.1:9050,32,disable_noise ${TOR_ADDRESS:+--anonymous-inbound=$TOR_ADDRESS:$TOR_PORT,127.0.0.1:$TOR_PORT} "
 fi
 
 if [ "$RPC_ENABLED" == "TRUE" ]; then
-	rpc_args="--rpc-restricted-bind-ip=\"$DEVICE_IP\" --rpc-restricted-bind-port=\"$RPC_PORT\" --rpc-login=\"$RPCu:$RPCp\" --rpc-ssl disabled "
+	rpc_args="--rpc-restricted-bind-ip=\"$DEVICE_IP\" --rpc-restricted-bind-port=\"$RPC_PORT\" ${RPCu:+--rpc-login=\"$RPCu:$RPCp\"} "
 fi
 
-eval /home/nodo/bin/monerod "$i2p_args$tor_args$rpc_args$cln_flags" --db-sync-mode="$SYNC_MODE" --data-dir="$DATA_DIR" --zmq-pub "tcp://$DEVICE_IP:18083" --confirm-external-bind --in-peers="$IN_PEERS" --out-peers="$OUT_PEERS" --limit-rate-up="$LIMIT_RATE_UP" --limit-rate-down="$LIMIT_RATE_DOWN" --max-log-file-size=10485760 --log-level=0 --max-log-files=1 --enable-dns-blocklist --p2p-bind-port="$MONERO_PORT" --rpc-bind-ip=0.0.0.0 --rpc-bind-port="$MONERO_PUBLIC_PORT" --non-interactive
+eval /home/nodo/bin/monerod "$i2p_args$tor_args$rpc_args$cln_flags" --db-sync-mode="$SYNC_MODE" --data-dir="$DATA_DIR" --zmq-pub tcp://"$DEVICE_IP":"$ZMQ_PUB" --in-peers="$IN_PEERS" --out-peers="$OUT_PEERS" --limit-rate-up="$LIMIT_RATE_UP" --limit-rate-down="$LIMIT_RATE_DOWN" --max-log-file-size=10485760 --log-level=0 --max-log-files=1 --enable-dns-blocklist --p2p-bind-port="$MONERO_PORT" --non-interactive
