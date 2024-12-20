6d4634580d1555d5b78c08979b2421c27409e2dd
interloper <15019654+intr-cx@users.noreply.github.com>
2024-12-20 12:13:07 +0100

    Expand banlist functionality include more fine-grained control

--- update-banlists.sh
+++ update-banlists.sh
@@ -2,8 +2,8 @@
 
 cd /media/monero/ || exit 1
 {
-	curl -LSs https://github.com/Boog900/monero-ban-list/raw/refs/heads/main/ban_list.txt
-	curl -LSs https://gui.xmr.pm/files/block.txt
+	[ "$(getvar "banlists.boog900")" == "TRUE" ] && curl -LSs https://github.com/Boog900/monero-ban-list/raw/refs/heads/main/ban_list.txt
+	[ "$(getvar "banlists.gui-xmr-pm")" == "TRUE" ] && curl -LSs https://gui.xmr.pm/files/block.txt
 } | sort -u > newbanlist && cp newbanlist banlist.txt
 
 chown monero:monero banlist.txt
